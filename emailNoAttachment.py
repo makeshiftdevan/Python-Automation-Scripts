837ed954515ca25f4c0fa60ee5da0d15141957a1
makeshiftdevan <64051402+makeshiftdevan@users.noreply.github.com>
2022-06-07 21:04:27 -0400

    Automated email without attachments
    Currently set up with gmail SMTP

---
+++ emailNoAttachment.py
@@ -0,0 +1,57 @@
+import datetime
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+
+fromaddr = "sendingEmailAddressGoesHere"
+toaddr = "recievingEmailAddressGoesHere"
+   
+# instance of MIMEMultipart 
+msg = MIMEMultipart() 
+  
+# storing the senders email address   
+msg['From'] = fromaddr 
+  
+# storing the receivers email address  
+msg['To'] = toaddr 
+  
+# storing the subject
+time_now = datetime.datetime.now().isoformat()
+msg['Subject'] = "Daily Attendance" + time_now[0:10]
+  
+# string to store the body of the mail 
+body = r"Today's attendance has been taken."
+  
+# attach the body with the msg instance 
+msg.attach(MIMEText(body, 'plain')) 
+  
+# instance of MIMEBase and named as p 
+p = MIMEBase('application', 'octet-stream') 
+  
+# encode into base64 
+#encoders.encode_base64(p) 
+   
+p.add_header('Content-Disposition', "attachment;None") 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login(fromaddr, "Wendell1!") 
+  
+# Converts the Multipart msg into a string 
+text = msg.as_string() 
+  
+# sending the mail 
+s.sendmail(fromaddr, toaddr, text) 
+  
+# terminating the session 
+s.quit()
+
+
+
